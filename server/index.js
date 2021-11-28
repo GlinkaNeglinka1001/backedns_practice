55309d4d2a058dd3aabb55c7a1fe1a7d2381c45b
Vlad <vzelenchukov2006@yandex.ru>
2021-11-28 13:38:05 +0300

    начало

---
+++ server/index.js
@@ -0,0 +1,19 @@
+// var express = require("express");
+// var app = express();
+// var cors = require("cors");
+// app.use(cors());
+// app.get("/", function(request, response){
+//     response.send("Hello, Node.js");
+// });
+// app.listen(591);
+
+var express = require("express");
+var app = express();
+var cors = require("cors");
+app.use(cors())
+
+app.get("/", function(request, response){
+    response.send("Hello, Node.js");
+});
+
+app.listen(591);
